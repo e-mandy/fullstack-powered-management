1bbdba7e90d69434df35cf12457e3e0415636f3c
e-mandy <andymfrd02@gmail.com>
2025-12-05 06:40:31 +0100

    Mise en place du model User

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,3 +1,45 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
-const UserModel = mongoose.Schema
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true,
+        trim: true,
+        minlength: [3, "Username muste be at least 3 characters long"]
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide an email'],
+        unique: true,
+        lowercase: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minLength: [6, 'Password must be at least 6 characters long']
+    },
+    profileImage: {
+        type: String,
+        default: null
+    }
+}, { timestamps: true });
+
+UserSchema.pre('save', async function(next){
+    if(!this.isModified('password')){
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
