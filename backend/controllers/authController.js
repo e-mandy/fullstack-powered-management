c21f46a9686a4ea93f9960cf45aedb27e352b4f9
e-mandy <andymfrd02@gmail.com>
2025-12-09 13:45:06 +0100

    Mise en place de la fonction de register

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -11,8 +11,45 @@ const generateToken = (id) => {
 
 // Mise en place de la logique de connexion
 export const register = async (req, res, next) => {
+    const { username, email, password } = req.body;
     try{
+        const userExists = await User.findOne({ $or: [{email}] });
 
+        if(userExists) {
+            return res.status(400).json({
+                success: false,
+                error: 
+                    userExists.email === email 
+                    ? "Email already registered"
+                    : "Username already taken",
+                statusCode: 400
+            });
+        }
+
+        // Create user
+        const user = User.create({
+            username,
+            email,
+            password
+        });
+
+        // Generate token
+        const token = generateToken(user._id);
+
+        res.status(201).json({
+            success: true,
+            data: {
+                user: {
+                    id: user._id,
+                    username: user.username,
+                    email: user.email,
+                    profileImage: user.profileImage,
+                    createdAt: user.createdAt
+                },
+                token,
+            },
+            message: "User registered successfully"
+        })
     }catch(error){
         next(error)
     }
@@ -20,20 +57,36 @@ export const register = async (req, res, next) => {
 
 // Logique de connexion
 export const login = async (req, res, next) => {
+    try{
 
+    }catch(error){
+        next(error)
+    }
 };    
 
 // Récupération des informations de profil d'un utilisateur
 export const getProfile = async (req, res, next) => {
+    try{
 
+    }catch(error){
+        next(error)
+    }
 };
 
 // Mise à jour du profil d'un utilisateur
 export const updateProfil = async (req, res, next) => {
+    try{
 
+    }catch(error){
+        next(error)
+    }
 };
 
 // Modification du mot de passe de l'utilisateur
 export const changePassword = async (req, res, next) => {
+    try{
 
+    }catch(error){
+        next(error)
+    }
 };
